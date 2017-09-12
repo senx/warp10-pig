8e23483a81e35386a478dcaf35a18a8933b85b11
David Morin <morin.david.bzh@gmail.com>
2017-09-13 00:22:57 +0200

    refactor GTSUpload to take a list of elements (bag) and accepts GTSWrapper wrapped in Base64 (OPB64)

--- src/main/java/io/warp10/pig/GTSUpload.java
+++ src/main/java/io/warp10/pig/GTSUpload.java
@@ -5,12 +5,18 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.zip.GZIPOutputStream;
 
+import com.google.common.base.Charsets;
 import com.google.common.util.concurrent.RateLimiter;
 import io.warp10.continuum.gts.GTSWrapperHelper;
 import io.warp10.continuum.store.Constants;
+import io.warp10.crypto.OrderPreservingBase64;
 import org.apache.pig.EvalFunc;
+import org.apache.pig.data.DataBag;
 import org.apache.pig.data.DataByteArray;
 import org.apache.pig.data.DataType;
 import org.apache.pig.data.Tuple;
@@ -29,9 +35,12 @@ import io.warp10.continuum.store.thrift.data.Metadata;
  */
 public class GTSUpload extends EvalFunc<Long> {
 
+  private static final String usageStr = "bytearray(GTSWrapper) OR chararray(Input Format) OR bag:{(bytearray)} OR bag:{(chararray)}";
+
   //
   // Limit data upload
   //
+
   private RateLimiter rateLimiter = null;
 
   private String params = null;
@@ -53,20 +62,30 @@ public class GTSUpload extends EvalFunc<Long> {
   public Long exec(Tuple input) throws IOException {
 
     //
+    // HttpURLConnection to Warp (update)
+    //
+
+    HttpURLConnection conn = null;
+
+    //
+    // Number of datapoints uploaded
+    //
+
+    long count = 0L;
+
+    //
     // GTSWrapper
     //
 
-    DataByteArray serialized = null;
+    List<DataByteArray> serialized = new ArrayList<>();
 
     //
     // We can provided GTS String representation directly (InputFormat)
     //
 
-    String gtsAsString = null;
+    List<String> gtsAsString = new ArrayList<>();
 
     String params = this.params;
-
-    reporter.progress();
 
     if (2 == input.size()) {
       params = input.get(0).toString();
@@ -75,40 +94,32 @@ public class GTSUpload extends EvalFunc<Long> {
     if ((0 == input.size()) || (input.size() > 2)) {
       throw new IOException("Invalid input, should be a tuple containing a GTS or parameters and GTS.");
     }
-    
-    if (DataType.BYTEARRAY == DataType.findType(input.get(input.size() - 1))) {
-      serialized = (DataByteArray) input.get(input.size() - 1);
-    } else if (DataType.CHARARRAY == DataType.findType(input.get(input.size() - 1))) {
-      gtsAsString = (String) input.get(input.size() - 1);
-    } else {
-      throw new IOException("Invalid input: bytearray(GTSWrapper) or chararray(Input Format)");
-    }
 
     //
     // Extract parameters
     //
-    
-    String[] tokens = params.split(" ");
-    
-    int i = 0;
-    
+
     String endpoint = null;
     String token = null;
     boolean gzip = false;
     String header = null;
     String rateLimit = null;
-    
+
+    String[] tokens = params.split(" ");
+
+    int i = 0;
+
     while (i < tokens.length) {
       if ("-t".equals(tokens[i])) {
         i++;
-        token = tokens[i];        
+        token = tokens[i];
       }
       if ("-u".equals(tokens[i])) {
         i++;
         endpoint = tokens[i];
       }
       if ("-c".equals(tokens[i])) {
-        gzip = true;        
+        gzip = true;
       }
       if ("-H".equals(tokens[i])) {
         i++;
@@ -128,11 +139,7 @@ public class GTSUpload extends EvalFunc<Long> {
       }
       i++;
     }
-    
-    HttpURLConnection conn = null;
-    
-    long count = 0L;
-    
+
     try {
       conn = (HttpURLConnection) new URL(endpoint).openConnection();
       conn.setRequestMethod("POST");
@@ -140,64 +147,79 @@ public class GTSUpload extends EvalFunc<Long> {
       conn.setDoInput(true);
       conn.setRequestProperty(null == header ? Constants.HTTP_HEADER_TOKEN_DEFAULT : header, token);
       conn.setChunkedStreamingMode(65536);
-      
+
       if (gzip) {
         conn.setRequestProperty("Content-Type", "application/gzip");
       }
+
       conn.connect();
-      
+
       OutputStream out = conn.getOutputStream();
-      
+
       if (gzip) {
         out = new GZIPOutputStream(out);
       }
-      
+
       PrintWriter pw = new PrintWriter(out);
 
-      if (DataType.BYTEARRAY == DataType.findType(input.get(input.size() - 1))) {
-        TDeserializer deserializer = new TDeserializer(new TCompactProtocol.Factory());
+      Object value = input.get(input.size() - 1);
 
-        GTSWrapper gtsWrapper = new GTSWrapper();
+      if (DataType.BYTEARRAY == DataType.findType(value)) {
+        count += uploadGtsWrapper((DataByteArray) value, pw);
+      } else if (DataType.CHARARRAY == DataType.findType(value)) {
 
-        try {
-          deserializer.deserialize(gtsWrapper, (serialized.get()));
-        } catch (TException te) {
-          throw new IOException(te);
+        String valueStr = (String) value;
+
+        //
+        // If the String contains a '/' this is Input Format
+        // Otherwise this a GTSWrapper wrapped as OrderPreservingBase64 (no '/')
+        //
+
+        if (valueStr.contains("/")) {
+          pw.println(valueStr);
+          count++;
+        } else {
+          byte[] bytes = OrderPreservingBase64.decode(valueStr.getBytes(Charsets.US_ASCII));
+          DataByteArray wrapperEncoded = new DataByteArray(bytes);
+          count += uploadGtsWrapper(wrapperEncoded, pw);
         }
 
-        Metadata metadataChunk = new Metadata(gtsWrapper.getMetadata());
+      } else if (DataType.BAG == DataType.findType(input.get(input.size() - 1))) {
+        DataBag inbag = (DataBag)input.get(0);
+        Iterator<Tuple> iterMain = inbag.iterator();
 
-        GTSDecoder decoder = GTSWrapperHelper.fromGTSWrapperToGTSDecoder(gtsWrapper);
-
-        StringBuilder metasb = new StringBuilder();
-        GTSHelper.metadataToString(metasb, metadataChunk.getName(),
-            metadataChunk.getLabels());
-
-        boolean first = true;
-
-        while (decoder.next()) {
+        while (iterMain.hasNext()) {
           reporter.progress();
 
-          if (null != rateLimiter) {
-            this.rateLimiter.acquire(1);
-          }
+          //
+          // One tuple with one field (bytearray or chararray)
+          //
 
-          if (!first) {
-            pw.print("=");
-            pw.println(GTSHelper.tickToString(null, decoder.getTimestamp(),
-                decoder.getLocation(), decoder.getElevation(),
-                decoder.getValue()));
+          Tuple inTuple = iterMain.next();
+          Object inValue = inTuple.get(0);
+
+          if (DataType.BYTEARRAY == DataType.findType(inValue)) {
+            count += uploadGtsWrapper((DataByteArray) inValue,pw);
+          } else if (DataType.CHARARRAY == DataType.findType(inValue)) {
+
+            String inValueStr = (String) inValue;
+
+            if (inValueStr.contains("/")) {
+              // Input Format
+              pw.println(inValueStr);
+              count++;
+            } else {
+              byte[] bytes = OrderPreservingBase64.decode(inValueStr.getBytes(Charsets.US_ASCII));
+              DataByteArray wrapperEncoded = new DataByteArray(bytes);
+              count += uploadGtsWrapper(wrapperEncoded, pw);
+            }
+
           } else {
-            pw.println(GTSHelper.tickToString(metasb, decoder.getTimestamp(),
-                decoder.getLocation(), decoder.getElevation(),
-                decoder.getValue()));
-            first = false;
+            throw new IOException("Invalid input: " + usageStr);
           }
-          count++;
         }
       } else {
-        pw.println(gtsAsString);
-        count++;
+        throw new IOException("Invalid input: " + usageStr);
       }
 
       pw.close();
@@ -211,6 +233,63 @@ public class GTSUpload extends EvalFunc<Long> {
       if (null != conn) { conn.disconnect(); }
     }
     
+    return count;
+  }
+
+  /**
+   * Upload a GTSWrapper through the PrintWriter (onto HTTP OutputStream)
+   * Returns the number of datapoints uploaded
+   *
+   * @param wrapperEncoded
+   * @param printWriter
+   * @return long
+   * @throws IOException
+   */
+  protected long uploadGtsWrapper(DataByteArray wrapperEncoded, PrintWriter printWriter) throws IOException {
+    long count = 0L;
+
+    TDeserializer deserializer = new TDeserializer(
+        new TCompactProtocol.Factory());
+
+    GTSWrapper gtsWrapper = new GTSWrapper();
+
+    try {
+      deserializer.deserialize(gtsWrapper, (wrapperEncoded.get()));
+    } catch (TException te) {
+      throw new IOException(te);
+    }
+
+    Metadata metadataChunk = new Metadata(gtsWrapper.getMetadata());
+
+    GTSDecoder decoder = GTSWrapperHelper
+        .fromGTSWrapperToGTSDecoder(gtsWrapper);
+
+    StringBuilder metasb = new StringBuilder();
+    GTSHelper.metadataToString(metasb, metadataChunk.getName(),
+        metadataChunk.getLabels());
+
+    boolean first = true;
+
+    while (decoder.next()) {
+      reporter.progress();
+
+      if (null != rateLimiter) {
+        this.rateLimiter.acquire(1);
+      }
+
+      if (!first) {
+        printWriter.print("=");
+        printWriter.println(GTSHelper.tickToString(null, decoder.getTimestamp(),
+            decoder.getLocation(), decoder.getElevation(),
+            decoder.getValue()));
+      } else {
+        printWriter.println(GTSHelper.tickToString(metasb, decoder.getTimestamp(),
+            decoder.getLocation(), decoder.getElevation(),
+            decoder.getValue()));
+        first = false;
+      }
+      count++;
+    }
     return count;
   }
 
