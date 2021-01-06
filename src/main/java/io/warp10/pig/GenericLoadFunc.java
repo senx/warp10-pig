47dcbc99b0322f01c78a235e6aeb5b9a6dae32a5
Mathias Herberts <Mathias.Herberts+git@gmail.com>
2021-01-06 13:22:37 +0100

    Added call to WarpScriptInputFormat#init to allow for multiple configs in a single pig script

--- src/main/java/io/warp10/pig/GenericLoadFunc.java
+++ src/main/java/io/warp10/pig/GenericLoadFunc.java
@@ -1,5 +1,5 @@
 //
-//   Copyright 2018  SenX S.A.S.
+//   Copyright 2018-2021  SenX S.A.S.
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
@@ -27,25 +27,27 @@ import org.apache.hadoop.mapreduce.InputFormat;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.RecordReader;
 import org.apache.pig.LoadFunc;
+import org.apache.pig.backend.hadoop.executionengine.mapReduceLayer.PigInputFormat;
 import org.apache.pig.backend.hadoop.executionengine.mapReduceLayer.PigSplit;
 import org.apache.pig.data.BinSedesTupleFactory;
 import org.apache.pig.data.Tuple;
 import org.apache.pig.data.TupleFactory;
 
+import io.warp10.hadoop.WarpScriptInputFormat;
 import io.warp10.hadoop.WritableUtils;
 import io.warp10.pig.utils.PigUtils;
 
 public class GenericLoadFunc extends LoadFunc {
-  
+
   private static final String PIG_GENERICLOAD_INPUTFORMAT = "pig.genericload.inputformat";
   private static final String PIG_GENERICLOAD_CONF_SUFFIX = "pig.genericload.conf.suffix";
-  
+
   private TupleFactory tfactory = new BinSedesTupleFactory();
-  
+
   private Job job = null;
   private final String suffix;
-  private RecordReader reader = null;  
-  
+  private RecordReader reader = null;
+
   static {
     PigWarpConfig.ensureConfig();
   }
@@ -55,23 +57,23 @@ public class GenericLoadFunc extends LoadFunc {
     //
     // Retrieve the class of the InputFormat we should create
     //
-    
+
     Configuration conf = this.job.getConfiguration();
-    
+
     String confsfx = conf.get(PIG_GENERICLOAD_CONF_SUFFIX + this.suffix, "");
-    
+
     if (!"".equals(confsfx)) {
       confsfx = "." + confsfx;
       List<Entry<String,String>> keys = new ArrayList<Entry<String,String>>();
       Iterator<Entry<String,String>> iter = conf.iterator();
       while(iter.hasNext()) {
         Entry<String,String> entry = iter.next();
-        
+
         if (entry.getKey().endsWith(confsfx)) {
           keys.add(entry);
         }
       }
-      
+
       // Override or create the unsuffixed configuration parameters
       for (Entry<String,String> entry: keys) {
         String key = entry.getKey().substring(0, entry.getKey().length() - confsfx.length());
@@ -80,7 +82,7 @@ public class GenericLoadFunc extends LoadFunc {
     }
 
     String inputFormat = conf.get(PIG_GENERICLOAD_INPUTFORMAT + this.suffix);
-        
+
     if (null == inputFormat) {
       inputFormat = conf.get(PIG_GENERICLOAD_INPUTFORMAT);
     }
@@ -88,17 +90,22 @@ public class GenericLoadFunc extends LoadFunc {
     try {
       Class ifclass = Class.forName(inputFormat);
       InputFormat format = (InputFormat) ifclass.newInstance();
+
+      // Special case for the WarpScriptInputFormat to set its suffix
+      if (format instanceof WarpScriptInputFormat) {
+        ((WarpScriptInputFormat) format).init(conf);
+      }
       return format;
     } catch (Exception e) {
       throw new IOException(e);
     }
   }
-  
+
   public GenericLoadFunc() {
     this.suffix = "";
   }
-  
-  
+
+
   public GenericLoadFunc(String suffix) {
     if (null != suffix) {
       if (!"".equals(suffix)) {
@@ -110,7 +117,7 @@ public class GenericLoadFunc extends LoadFunc {
       this.suffix = "";
     }
   }
-  
+
   @Override
   public Tuple getNext() throws IOException {
     try {
@@ -120,11 +127,11 @@ public class GenericLoadFunc extends LoadFunc {
 
       Object key = this.reader.getCurrentKey();
       Object value = this.reader.getCurrentValue();
-      
+
       if (key instanceof Writable) {
         key = WritableUtils.fromWritable((Writable) key);
       }
-      
+
       if (value instanceof Writable) {
         value = WritableUtils.fromWritable((Writable) value);
       }
@@ -139,14 +146,14 @@ public class GenericLoadFunc extends LoadFunc {
       throw new IOException(ie);
     }
   }
-  
+
   @Override
   public void prepareToRead(RecordReader reader, PigSplit split) throws IOException {
     this.reader = reader;
   }
- 
+
   @Override
   public void setLocation(String location, Job job) throws IOException {
     this.job = job;
-  }  
+  }
 }
